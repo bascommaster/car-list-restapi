dec95645c7ada1604d856ec53e4f5b44937f5058
Mirosław Malik <mmiro390@gmail.com>
2020-01-17 22:30:01 +0100

    add start configuration pom.xml and needed packages

---
+++ src/main/java/com/bascommaster/springcar/SpringcarApplication.java
@@ -0,0 +1,13 @@
+package com.bascommaster.springcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bascommaster/springcar/SpringcarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bascommaster.springcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
